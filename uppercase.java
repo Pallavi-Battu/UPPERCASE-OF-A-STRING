da0ea54b0205f393e479e96a94d89d9c0c600bf7
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-28 18:12:45 +0530

    Add files via upload

---
+++ uppercase.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class uppercase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        String upperString = inputString.toUpperCase();
+        System.out.println("Uppercase String: " + upperString);
+        scanner.close();
+    }
+}
+
+    
+
